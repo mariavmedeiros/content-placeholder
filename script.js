9e9c47c26ab424d9da253801b363bd92374c1b71
Maria Medeiros <maitevelori@hotmail.com>
2022-12-25 18:20:47 -0800

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile-img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80"alt=""/>';
+}
